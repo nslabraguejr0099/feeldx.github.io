dcdc7a339d533fff4e50fe2b2ac103fabc602230
nslabraguejr0099 <nslabraguejr@gmail.com>
2025-07-05 14:07:33 +0800

    first commit

---
+++ js/router.js
@@ -0,0 +1,30 @@
+var app_url = 'http://localhost/apps/feeldx/';
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+console.log(event.target.href);
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404:     "pages/404.html",
+    "":      "pages/index.html",
+    "index": "pages/index.html",
+    "about": "pages/about.html",
+    "lorem": "pages/lorem.html",
+};
+
+const handleLocation = async () => {
+    const path = window.location.href.replace(app_url, '');
+console.log(path);
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
