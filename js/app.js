96c772c3269bbbf3aacfe272f4a21bc79bb813bd
nslabraguejr0099 <nslabraguejr@gmail.com>
2025-07-07 15:56:47 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
-window.addEventListener('load', function () {
-    document.getElementById('main-page').innerHTML = '<i class="fas fa-yen-sign" style="height: 20px; width: 20px;"></i>';
-});
+var heroInterval;
+
+$('#nav-bar').load('nav.html');
+$('#footer').load('footer.html');
--- js/router.js
+++ js/router.js
@@ -1,26 +1,25 @@
-var app_url = 'https://nslabraguejr0099.github.io/feeldx.github.io/';
-// var app_url = 'http://localhost/apps/feeldx/';
+// var app_url = 'https://nslabraguejr0099.github.io/feeldx.github.io/';
+var app_url = 'http://localhost/apps/feeldx/';
 
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
-console.log(event.target.href);
     window.history.pushState({}, "", event.target.href);
     handleLocation();
 };
 
 const routes = {
-    404:     "pages/404.html",
-    "":      "pages/index.html",
-    "index": "pages/index.html",
-    "sample": "pages/sample.html",
+    404:       "pages/404.html",
+    "":        "pages/index.html",
+    "index":   "pages/index.html",
+    "about":   "pages/about.html",
+    "contact": "pages/contact.html",
 };
 
 const handleLocation = async () => {
     const path = window.location.href.replace(app_url, '');
     const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+    $('#main-page').load(route);
 };
 
 window.onpopstate = handleLocation;
